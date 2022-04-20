f3afe722d2541599ceca7198a34f4c9819ea9c20
Vitória Souza <vitoria.souza@acct.global>
2022-04-19 22:49:39 -0300

    added useState in form customer field

--- src/pages/Dashboard/Tickets/New/NewTicket.jsx
+++ src/pages/Dashboard/Tickets/New/NewTicket.jsx
@@ -1,13 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { FiPlusCircle } from "react-icons/fi";
+import { AuthContext } from "../../../../contexts/Auth";
+import firebase from "../../../../services/firebaseConnection";
 import Header from "../../../../components/Header/Header";
 import Title from "../../../../components/Title/Title";
 import "./styles.css";
 
 export default function NewTicket() {
+  const { user } = useContext(AuthContext);
+  const [loadingCustomers, setLoadingCustomers] = useState(true);
+  const [customers, setCustomers] = useState([]);
+  const [customerSelected, setCustomerSelected] = useState(0);
   const [subject, setSubject] = useState("Suporte");
   const [status, setStatus] = useState("Aberto");
   const [complement, setComplement] = useState("");
+
+  useEffect(() => {
+    async function loadCustomers() {
+      await firebase
+        .firestore()
+        .collection("customers")
+        .get()
+        .then(snapshot => {
+          let list = [];
+          snapshot.forEach(doc => {
+            list.push({
+              id: doc.id,
+              nomeFantasia: doc.data().nomeFantasia,
+            });
+          });
+
+          if (list.length === 0) {
+            console.log("Nenhuma empresa encontrada");
+            setCustomers([{ id: "1", nomeFantasia: "" }]);
+            setLoadingCustomers(false);
+            return;
+          }
+
+          setCustomers(list);
+          setLoadingCustomers(false);
+        })
+        .catch(error => {
+          console.log(error);
+          setLoadingCustomers(false);
+          setCustomers([{ id: "1", nomeFantasia: "" }]);
+        });
+    }
+    loadCustomers();
+  }, []);
 
   const handleRegister = e => {
     e.preventDefault();
@@ -19,6 +59,10 @@ export default function NewTicket() {
 
   const handleOptionChange = e => {
     setStatus(e.target.value);
+  };
+
+  const handleChangeCustomers = e => {
+    setCustomerSelected(e.target.value);
   };
 
   return (
@@ -33,11 +77,22 @@ export default function NewTicket() {
           <form className="form-profile" onSubmit={handleRegister}>
             <label>
               Cliente <br />
-              <select>
-                <option key={1} value={1}>
-                  Escola
-                </option>
-              </select>
+              {loadingCustomers ? (
+                <input type="text" value="Carregando clientes..." disabled />
+              ) : (
+                <select
+                  value={customerSelected}
+                  onChange={handleChangeCustomers}
+                >
+                  {customers.map((item, index) => {
+                    return (
+                      <option key={item.id} value={index}>
+                        {item.nomeFantasia}
+                      </option>
+                    );
+                  })}
+                </select>
+              )}
             </label>
             <label>
               Assunto <br />
